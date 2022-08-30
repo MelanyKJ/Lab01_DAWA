6e944ddef020f8f6359de6f35dd10a544f970f69
Melany <melanyac260@gmail.com>
2022-08-29 19:56:19 -0500

    lab01_first

---
+++ index.js
@@ -0,0 +1,69 @@
+import express from "express";
+
+const app = express()
+
+app.use(express.json());
+
+const people = [];
+
+app.get("/", (request, response)=>{
+
+        return response.json({
+            ok: true,
+            data: people,
+        })
+
+});
+
+app.post("/create", function(req, res){
+
+    const data = req.body;
+    data.id = people.length + 1;
+    people.push(req.body);
+
+    return res.status(201).json({
+        ok: true,
+        data: "Create",
+    })
+
+})
+
+//UPDATE
+app.put("/update/", (req, res)=>{
+    const data = req.body;
+    people[data.id-1].nombre=data.nombre;
+    people[data.id-1].apellido=data.apellido;
+
+    return res.json({
+        ok: true,
+        data: "Update",
+    })
+});
+
+//DELETE PERSON
+/*
+app.delete('/delete/:id', (req,res) =>{
+    const data = req.params.id;
+    let deletePerson;
+    people.forEach((item, i) => {
+      if(item.id == id){
+        deletePerson = people.splice(i,i + 1);
+      }
+    });
+
+*/
+
+app.delete('/delete/:id', (req,res) =>{
+    const data = req.params.id;
+    const index = people.findIndex((perDelete => perDelete.id==data));
+    people.splice(index,1);
+
+    return res.json({
+        ok:true,
+        data: "Delete"
+    })
+})
+
+app.listen(6004, () =>
+ console.log(`El servidor inicio en http://localhost:6004`)
+ )
